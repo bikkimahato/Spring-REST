0e54f6fca29d2906fd8daeb9fc243b140e360860
bikkimahato <bikkimahato123@gmail.com>
2020-08-10 12:25:40 +0530

    PathVariable for REST Endpoints

--- spring-rest-demo/src/main/java/com/bikki/springdemo/rest/StudentRestController.java
+++ spring-rest-demo/src/main/java/com/bikki/springdemo/rest/StudentRestController.java
@@ -3,7 +3,10 @@ package com.bikki.springdemo.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,15 +16,27 @@ import com.bikki.springdemo.entity.Student;
 @RequestMapping("/api")
 public class StudentRestController {
 
-	@GetMapping("/students")
-	public List<Student> getStudents() {
+	private List<Student> theStudents;
 
-		List<Student> theStudents = new ArrayList<>();
+	@PostConstruct
+	public void loadData() {
+
+		theStudents = new ArrayList<>();
 
 		theStudents.add(new Student("Bikki", "Mahato"));
 		theStudents.add(new Student("John", "Cena"));
 		theStudents.add(new Student("Hayden", "Jack"));
+	}
+
+	@GetMapping("/students")
+	public List<Student> getStudents() {
 
 		return theStudents;
+	}
+
+	@GetMapping("/students/{studentId}")
+	public Student getStudent(@PathVariable int studentId) {
+
+		return theStudents.get(studentId);
 	}
 }
